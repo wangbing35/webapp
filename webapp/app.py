bf23636c85fcc3ed67b2c9bdc496aeb63faa8ec1
wangbing35 <843345667@qq.com>
2017-04-05 17:53:41 +0800

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1,15 +1,44 @@
 import os
-
-from flask import Flask
-
+from flask import Flask,request,Response,send_file
+from PIL import Image,ImageDraw,ImageFont
+import requests
+import time
 app = Flask(__name__)
+#UPLOAD_DIR = '/tmp/upload'
+UPLOAD_DIR = 'C:/Users/w00412055/Desktop/'
+def allowed_file(file_name):
+    return file_name.split('.')[-1] in ['jpg','JPG','jpeg','JPEG','png','PNG','gif','GIF']
 
-@app.route('/')
-def hello():
-    provider = str(os.environ.get('PROVIDER', 'world'))
-    return 'HELLO '+provider+'!'
+def AddWaterMark(ImagePath):
+    img = Image.open(ImagePath)
+    draw = ImageDraw.Draw(img)
+    myfont = ImageFont.truetype('C:/windows/fonts/Arial.ttf', size=40)
+    fillcolor = '#ff0000'
+    draw.text((2,2), 'HUAWEI', font=myfont, fill=fillcolor)
+    img.save('C:/Users/w00412055/Desktop/result.jpg', 'jpeg')
+    return 'C:/Users/w00412055/Desktop/result.jpg'
+    
+@app.route('/upload_file',methods = ['GET','POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            ImagePath = os.path.join(UPLOAD_DIR,file.filename)
+            file.save(ImagePath)
+            filename = AddWaterMark(ImagePath)
+            return send_file(filename,mimetype='image/jpeg')
+        return "<p>file type not allowed!</p>"
+    return '''
+    <!DOCTYPE html>
+    <title>Change new icon</title>
+    <h1>Upload new </h1>
+    <form action = "" method = "post" enctype=multipart/form-data>
+        <input type = "file" name = file>
+        <input type = "submit" value = Upload>
+    </form>
+    '''
+
+  
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=5001,debug=True)
