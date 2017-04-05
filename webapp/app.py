134b5f91f09b63aa70e75935784be17727eb890a
wangbing35 <843345667@qq.com>
2017-04-05 17:54:17 +0800

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1,31 +1,28 @@
-import os
+import os,uuid
 from flask import Flask,request,Response,send_file
 from PIL import Image,ImageDraw,ImageFont
-import requests
-import time
 app = Flask(__name__)
-#UPLOAD_DIR = '/tmp/upload'
-UPLOAD_DIR = 'C:/Users/w00412055/Desktop/'
-def allowed_file(file_name):
-    return file_name.split('.')[-1] in ['jpg','JPG','jpeg','JPEG','png','PNG','gif','GIF']
 
-def AddWaterMark(ImagePath):
-    img = Image.open(ImagePath)
+def allowed_file(filename):
+    return filename.split('.')[-1] in ['jpg','JPG','jpeg','JPEG','png','PNG','gif','GIF']
+
+def AddWaterMark(filename):
+    img = Image.open(filename)
     draw = ImageDraw.Draw(img)
-    myfont = ImageFont.truetype('C:/windows/fonts/Arial.ttf', size=40)
+    myfont = ImageFont.truetype('./BASKVILL.TTF', size=20)
     fillcolor = '#ff0000'
     draw.text((2,2), 'HUAWEI', font=myfont, fill=fillcolor)
-    img.save('C:/Users/w00412055/Desktop/result.jpg', 'jpeg')
-    return 'C:/Users/w00412055/Desktop/result.jpg'
+    watermarked_file = uuid.uuid1().hex + filename
+    img.save(watermarked_file, 'jpeg')
+    return watermarked_file
     
 @app.route('/upload_file',methods = ['GET','POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
-            ImagePath = os.path.join(UPLOAD_DIR,file.filename)
-            file.save(ImagePath)
-            filename = AddWaterMark(ImagePath)
+            file.save(file.filename)
+            filename = AddWaterMark(file.filename)
             return send_file(filename,mimetype='image/jpeg')
         return "<p>file type not allowed!</p>"
     return '''
@@ -38,7 +35,13 @@ def upload_file():
     </form>
     '''
 
+@app.route('/health')
+def health_check():
+    return 'ok'
   
+@app.route('/')
+def health_check():
+    return 'Hello'
 
 if __name__ == '__main__':
-    app.run(port=5001,debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
